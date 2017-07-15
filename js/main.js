4f90c072348fa5821d00a572e77cb697804bd2ae
Alex Young <alexyoung1992@gmail.com>
2017-07-15 16:53:26 -0400

    feat(main.js): implement accordion for services

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function($) {
+    let accordionSpeed = 'fast'
     $('.content').find('.accordion-toggle').click(function(){
-        $(this).next().slideToggle('fast');
-        $(".accordion-content").not($(this).next()).slideUp('fast');
-    });
-});
+        $(this).next().slideToggle(accordionSpeed)
+        $(".accordion-content").not($(this).next()).slideUp(accordionSpeed)
+    })
+    $('.services').find('section').click(function() {
+        let contentSelector = '.package-copy'
+        let content = $(this).children(contentSelector)
+        content.slideToggle(accordionSpeed)
+        $(contentSelector).not(content).slideUp(accordionSpeed)
+    })
+})
