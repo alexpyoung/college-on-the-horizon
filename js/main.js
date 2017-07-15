8eee04d16798344528fe127106cd52c42604bcc6
Alex Young <alexyoung1992@gmail.com>
2017-07-15 16:43:37 -0400

    feat(main.js): simple accordion functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-
+$(document).ready(function($) {
+    $('.content').find('.accordion-toggle').click(function(){
+        $(this).next().slideToggle('fast');
+        $(".accordion-content").not($(this).next()).slideUp('fast');
+    });
+});
